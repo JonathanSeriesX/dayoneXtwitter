bb05499d82449b15205c1b9ef42be00c7e7a976c
Jonathunky <12529409+JonathanSeriesX@users.noreply.github.com>
2025-06-28 18:15:52 +0400

    Immolate improved

--- config.py
+++ config.py
@@ -7,14 +7,16 @@ Defines paths to the Twitter archive and Day One journal details.
 
 script_dir = os.path.dirname(os.path.abspath(__file__))
 TWEET_ARCHIVE_PATH = os.path.join(script_dir, "archive", "data", "tweets.js")
-STATUSES_FILE_PATH = os.path.join(script_dir, "statuses.txt") # Path to store processed tweet IDs
+STATUSES_FILE_PATH = os.path.join(
+    script_dir, "statuses.txt"
+)  # Path to store processed tweet IDs
 
 
-JOURNAL_NAME = "Twitter Test"
-REPLY_JOURNAL_NAME = "Twitter Replies Test" # Set to a journal name (e.g., "Twitter Replies") to post replies there, or None to ignore them
-CURRENT_USERNAME = "JonathanSeriesX" # Set to yours, otherwise links will break
-MAX_THREADS_TO_PROCESS = 200 # Set to an integer to limit the number of threads processed, or None for no limit
-SHUFFLE_MODE = True # Otherwise, we start with the oldest one
+JOURNAL_NAME = "Tweets"
+REPLY_JOURNAL_NAME = "Twitter Replies"  # Set to a journal name (e.g., "Twitter Replies") to post replies there, or None to ignore replies altogether
+CURRENT_USERNAME = "JonathanSeriesX"  # Set to yours, otherwise links will break
+MAX_THREADS_TO_PROCESS = None  # Set to an integer to limit the number of threads processed, or None for no limit
+SHUFFLE_MODE = True  # Otherwise, we start with the oldest one
 IGNORE_RETWEETS = False
 
 # Date range for processing tweets. Only threads started between these two dates will be processed.
@@ -24,9 +26,13 @@ END_DATE = "20 April 2069"
 
 
 # LLM Configuration for Ollama
-PROCESS_TITLES_WITH_LLM = True # Set to True to enable LLM-based title generation
+PROCESS_TITLES_WITH_LLM = True  # Set to True to enable LLM-based title generation
 OLLAMA_API_URL = os.getenv("OLLAMA_API_URL", "http://localhost:11434/api/generate")
-OLLAMA_MODEL_NAME = os.getenv("OLLAMA_MODEL_NAME", "llama3.1") # Default is llama3.1, can be configured
-OLLAMA_PROMPT = ("Figure out, what subject this tweet is about. Deliver very short answer in lowercase, "
-                 "like \"about weather\" or \"about Formula 1\". "
-                 "Only output few lowercase words, nothing else.") # tweet itself comes right afterwards
+OLLAMA_MODEL_NAME = os.getenv(
+    "OLLAMA_MODEL_NAME", "llama3.1"
+)  # Default is llama3.1, can be configured
+OLLAMA_PROMPT = (
+    "Figure out, what subject this tweet is about. Deliver very short answer in lowercase, "
+    'like "about weather" or "about Formula 1". '
+    "Only output few lowercase words, nothing else."
+)  # tweet itself comes right afterwards
