c9adaf42d7eda4682319a84b483d54180e743a29
Jonathunky <12529409+JonathanSeriesX@users.noreply.github.com>
2025-06-28 16:15:13 +0400

    Improved debug mode

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os
+
 import config
 from processing_utils import (
     load_processed_tweet_ids,
@@ -16,6 +18,7 @@ def main():
     if debug_tweet_ids:
         print(f"Debug mode: Processing {len(debug_tweet_ids)} specific tweets.")
         threads = load_and_prepare_threads(tweet_ids_to_debug=debug_tweet_ids)
+
     else:
         threads = load_and_prepare_threads()
 
@@ -28,6 +31,9 @@ def main():
             break
         process_single_thread(thread, processed_tweet_ids)
 
+    if debug_tweet_ids:
+        if os.path.isfile(config.STATUSES_FILE_PATH):
+            os.remove(config.STATUSES_FILE_PATH)
 
 if __name__ == "__main__":
     main()
