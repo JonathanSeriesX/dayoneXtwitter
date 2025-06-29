c1eb0dc488c100f466af686fe2a7de3168a5a2f3
Jonathunky <12529409+JonathanSeriesX@users.noreply.github.com>
2025-06-29 04:49:18 +0400

    Minor cleanup of config

--- config.py
+++ config.py
@@ -1,22 +1,13 @@
 import os
 
-"""
-Configuration settings for the Day One Twitter integration.
-Defines paths to the Twitter archive and Day One journal details.
-"""
-
-script_dir = os.path.dirname(os.path.abspath(__file__))
-TWEET_ARCHIVE_PATH = os.path.join(script_dir, "archive", "data", "tweets.js")
-STATUSES_FILE_PATH = os.path.join(
-    script_dir, "statuses.txt"
-)  # Path to store processed tweet IDs
-
+TWEET_ARCHIVE_PATH = "archive/data/tweets.js"
+STATUSES_FILE_PATH = "statuses.txt"
 
 JOURNAL_NAME = "Tweets"
 REPLY_JOURNAL_NAME = "Twitter Replies"  # Set to a journal name (e.g., "Twitter Replies") to post replies there, or None to ignore replies altogether
 CURRENT_USERNAME = "JonathanSeriesX"  # Set to yours, otherwise links will break
 MAX_THREADS_TO_PROCESS = None  # Set to an integer to limit the number of threads processed, or None for no limit
-SHUFFLE_MODE = True  # Otherwise, we start with the oldest one
+SHUFFLE_MODE = True  # Otherwise, we start from the oldest one
 IGNORE_RETWEETS = False
 
 # Date range for processing tweets. Only threads started between these two dates will be processed.
@@ -30,9 +21,9 @@ PROCESS_TITLES_WITH_LLM = True  # Set to True to enable LLM-based title generati
 OLLAMA_API_URL = os.getenv("OLLAMA_API_URL", "http://localhost:11434/api/generate")
 OLLAMA_MODEL_NAME = os.getenv(
     "OLLAMA_MODEL_NAME", "llama3.1"
-)  # Default is llama3.1, can be configured
+)
 OLLAMA_PROMPT = (
     "Figure out, what subject this tweet is about. Deliver very short answer in lowercase, "
     'like "about weather" or "about Formula 1". '
     "Only output few lowercase words, nothing else."
-)  # tweet itself comes right afterwards
+)  # the tweet itself comes right afterwards
