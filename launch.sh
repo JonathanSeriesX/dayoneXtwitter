61bbdc75a3b997f44783d412faad250aee02f2ee
Jonathunky <12529409+JonathanSeriesX@users.noreply.github.com>
2025-06-30 20:39:17 +0000

    Slightly nicer initialisation

--- launch.sh
+++ launch.sh
@@ -1,6 +1,6 @@
 xcode-select -p > /dev/null 2>&1 || xcode-select --install
 [ -d venv ] || python3 -m venv venv
 source venv/bin/activate
-pip3 install --upgrade pip
-pip3 install -r requirements.txt
+pip3 install --upgrade pip --quiet
+pip3 install -r requirements.txt --quiet
 python3 main.py
