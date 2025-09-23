9f18ce0e8dcc809185083957adeb78ccd4ef6191
JonathanSeriesX <12529409+JonathanSeriesX@users.noreply.github.com>
2025-09-23 18:44:31 +0100

    Minor updates

--- config.py
+++ config.py
@@ -1,9 +1,11 @@
 JOURNAL_NAME = "Tweets"
-REPLY_JOURNAL_NAME = "Twitter Replies"  # Journal for replies, or None to skip replies altogether
+REPLY_JOURNAL_NAME = (
+    "Twitter Replies"  # Journal for replies, or None to skip replies altogether
+)
 CURRENT_USERNAME = "JonathanSeriesX"  # Set to your current username. Or set to None if you've deleted your Twitter account forever.
 MAX_THREADS_TO_PROCESS = None  # Max threads to process, or None for no limit
 SHUFFLE_MODE = True  # True to shuffle threads; False to start from oldest
-IGNORE_RETWEETS = False # True to skip retweets entirely
+IGNORE_RETWEETS = False  # True to skip retweets entirely
 
 # Date range for processing tweets. Only threads started between these two dates will be processed.
 # Format: "DD Month YYYY" (e.g., "21 March 2006")
@@ -13,7 +15,7 @@ END_DATE = "20 April 2069"
 # LLM Configuration for Ollama
 PROCESS_TITLES_WITH_LLM = True  # Enable LLM-generated titles
 OLLAMA_API_URL = "http://localhost:11434/api/generate"
-OLLAMA_MODEL_NAME = "llama3.1"
+OLLAMA_MODEL_NAME = "qwen3"
 
 OLLAMA_PROMPT = (
     "Figure out, what subject this tweet is about. Deliver very short answer, "
